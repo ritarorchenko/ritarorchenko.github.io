ff346d1fa70cb3ad769296e58991968ea0d4cd0e
Rita Rorchenko <r.rorchenko@gmail.com>
2015-12-05 15:54:31 +0200

    hommie

---
+++ javascript/lesson2/task1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ritok on 14.10.2015.
+ */
+/**Task1*/
+var year = prompt( 'What year is it now?', '' );
+if (year == 2015){
+    alert( 'You are right!' );}
+else{
+    alert ( 'Are you crazy? 2015!' );}
+
+
+
